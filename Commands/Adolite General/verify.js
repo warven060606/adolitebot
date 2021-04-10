bca08a84da40ab3664b3eb5a534d7a8e09b05ad2
warven060606 <58671109+warven060606@users.noreply.github.com>
2021-04-10 20:39:41 +0200

    Update verify.js

--- Commands/Adolite General/verify.js
+++ Commands/Adolite General/verify.js
@@ -35,71 +35,7 @@ class Verify extends Command {
     async run(message, args) {
 
         // —— Database connection
-        const { USER, DB, MDP, HOST } = this.client.config.bdd;
-        var database = mysql.createConnection({ host: HOST, user: USER, password: MDP, database: DB, charset: "utf8mb4", });
-        
-        console.log(`[Adolite]:${message.author.tag}` + ` Utilisation => ` + `verify`);
-
-        if (DbConnection()) {
-            message.delete();
-            database.query(
-                'SELECT username,discord_id,discord_verify FROM users WHERE discord_token = ?',
-                [args.join(' ')],
-                async function (err, results) {
-                    if(err) return console.log(err) && message.channel.send('```diff\n- Une erreur est survenue, reéssayez ultérieurment !```');
-                    if (!results) return console.log("Aucun résultat");
-
-                    if (results.length > 0) {
-                        let username = results[0].username;
-                        let verify = results[0].discord_verify;
-                        let id = results[0].discord_id;
-
-                        if (message.author.id !== id) {
-                            message.channel.send('Le token que vous avez saisis, ne correspond pas à votre compte !');
-                            console.log(`${message.author.username}#${message.author.discriminator}(${message.author.id}) a essayé d'utiliser un token qui ne lui appartener pas.`);
-                            return;
-                        }
-
-
-                        if (verify > 0) return console.log(err) && message.channel.send('Ton compte est déja lié à Adolite !');
-
-                        database.query(
-                            'UPDATE users SET discord_verify = ?, discord_token = ? WHERE username = ?',
-                            ['1', '', username],
-                            function (err, results) {
-                                if(err) return message.channel.send('```diff\n- Une erreur est survenue, reéssayez ultérieurement !```');
-                                console.log(`Le compte de ${username} a été vérifié !`);
-                                Dbdisconnect();
-                            }
-                        );
-                        try {
-                            message.member.setNickname(username);
-                            message.member.roles.add('735484468126810201');
-                        } catch (e){
-                            console.log(e);
-                        }
-
-                        let Embed = new MessageEmbed()
-                            .setTitle(`Bravo ${username} !`)
-                            .setColor("GREEN")
-                            .setDescription("Ton compte discord est désormais lié à ton compte Adolite ! ✅")
-                            .setTimestamp();
-
-                        await message.channel.send(Embed);
-                    } else {
-                        Dbdisconnect();
-
-                        let Embed = new MessageEmbed()
-                            .setTitle(`Oops ${message.author.username}!`)
-                            .setColor("RED")
-                            .setDescription("Vérifier que les caractères que vous avez saisis correspondent à ceux du site ! ❌")
-                            .setTimestamp();
-                        return message.channel.send(Embed);
-                    }
-                    if (err) return console.log(err);
-
-                }
-            );
+       
         }
         database.on('error', function () {
             DbConnection();
